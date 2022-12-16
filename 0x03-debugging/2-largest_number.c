156c27d9680673dd180eafa448d6457398311e38
EzekielNwafor <nwaforezekiel421@gmail.com>
2022-12-15 18:34:31 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,23 +9,23 @@
  */
 int largest_mumber(int a, int b, int c)
 {
-	int largest;
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
-	}
+int largest;
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-	return (largest};
+return (largest};
 }
