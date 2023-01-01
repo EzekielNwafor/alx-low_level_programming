134cd1927ec343052411a3ff2bc603483254a929
EzekielNwafor <nwaforezekiel421@gmail.com>
2023-01-01 14:39:52 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit.c - check if numbers are o - 9
+ * @c: char to be checked
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit.c(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
